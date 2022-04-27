ab4a3dca7a6b0f8068f6dd81785a92e59efd8102
devsaasha <developersaasha@gmail.com>
2022-04-27 18:37:26 +0600

    asn07

---
+++ function.js
@@ -0,0 +1,276 @@
+
+/**- Create a Student Array & Object Data Structure ( name, roll, age, class, gender, location, Admission fees ) 30.  now create a console data table with */
+
+const studentObjectData =[
+  {
+     name : 'S A Asha',
+     roll : 1,
+     age : 10,
+     class : 'Five',
+     gender : 'Female',
+     Location : 'Jamil Nagor',
+     Admission_fee : 1500
+  },
+  {
+     name : 'Tithi paul',
+     roll : 2,
+     age : 12,
+     class : 'Five',
+     gender : 'Female',
+     Location : 'Jamil Nagor',
+     Admission_fee : 1500
+  },
+  {
+     name : 'Ritu Bormon',
+     roll : 3,
+     age : 15,
+     class : 'Seven',
+     gender : 'Female',
+     Location : 'Johurul Nagor',
+     Admission_fee : 1700
+  },
+  {
+     name : 'Arman Rafi',
+     roll : 4,
+     age : 22,
+     class : 'Nine',
+     gender : 'Malea',
+     Location : 'Jamil Nagor',
+     Admission_fee : 1900
+  },
+  {
+     name : 'Sohag Candra',
+     roll : 5,
+     age : 25,
+     class : 'Ten',
+     gender : 'Male',
+     Location : 'Seusgari',
+     Admission_fee : 2000
+  },
+  {
+     name : 'Sima Akter',
+     roll : 6,
+     age : 30,
+     class : 'Six',
+     gender : 'Female',
+     Location : 'Kamargari',
+     Admission_fee : 1600
+  },
+  {
+     name : 'Munir khan',
+     roll : 7,
+     age : 24,
+     class : 'Five',
+     gender : 'Male',
+     Location : 'Jamil Nagor',
+     Admission_fee : 1500
+  },
+  {
+     name : 'Mahid Islam',
+     roll : 8,
+     age : 28,
+     class : 'Ten',
+     gender : 'Male',
+     Location : 'Seusgari',
+     Admission_fee : 2000
+  },
+  {
+     name : 'Sawon Roy',
+     roll : 9,
+     age : 13,
+     class : 'Six',
+     gender : 'Female',
+     Location : 'Jamil Nagor',
+     Admission_fee : 1500
+  },
+  {
+     name : 'Biplop kumar',
+     roll : 10,
+     age : 12,
+     class : 'Five',
+     gender : 'Male',
+     Location : 'Jamil Nagor',
+     Admission_fee : 1500
+  },
+  {
+     name : 'Sriti Akter',
+     roll : 11,
+     age : 28,
+     class : 'Ten',
+     gender : 'Female',
+     Location : 'Kamargari',
+     Admission_fee : 2000
+  },
+  {
+     name : 'Afiya Afi',
+     roll : 12,
+     age : 15,
+     class : 'Six',
+     gender : 'Female',
+     Location : 'Kamargari',
+     Admission_fee : 1600
+  },
+  {
+     name : 'Tusha Chawdhory',
+     roll : 13,
+     age : 22,
+     class : 'Eight',
+     gender : 'Female',
+     Location : 'Johurul Nagor',
+     Admission_fee : 1800
+  },
+  {
+     name : 'Sumona Rani',
+     roll : 14,
+     age : 14,
+     class : 'Five',
+     gender : 'Female',
+     Location : 'Jamil Nagor',
+     Admission_fee : 1500
+  },
+  {
+     name : 'Redwon Mondol',
+     roll : 15,
+     age : 19,
+     class : 'Five',
+     gender : 'Male',
+     Location : 'Jamil Nagor',
+     Admission_fee : 1500
+  },
+  {
+     name : 'Zakariya Hossien',
+     roll : 16,
+     age : 27,
+     class : 'Nine',
+     gender : 'Male',
+     Location : 'Johurul Nagor',
+     Admission_fee : 1900
+  },
+  {
+     name : 'Partho Paul',
+     roll : 17,
+     age : 22,
+     class : 'Nine',
+     gender : 'Male',
+     Location : 'Jamil Nagor',
+     Admission_fee : 1900
+  },
+  {
+     name : 'Milon Hossen',
+     roll : 18,
+     age : 23,
+     class : 'Eight',
+     gender : 'Maleale',
+     Location : 'Jamil Nagor',
+     Admission_fee : 1800
+  },
+  {
+     name : 'Mim Akter',
+     roll : 19,
+     age : 28,
+     class : 'Ten',
+     gender : 'Female',
+     Location : 'Jamil Nagor',
+     Admission_fee : 2000
+  },
+  {
+     name : 'Nishat Muni',
+     roll : 20,
+     age : 19,
+     class : 'Six',
+     gender : 'Female',
+     Location : 'Kamargari',
+     Admission_fee : 1600
+  },
+  {
+     name : 'Nourin Akter',
+     roll : 21,
+     age : 25,
+     class : 'Five',
+     gender : 'Female',
+     Location : 'Jamil Nagor',
+     Admission_fee : 1500
+  },
+  {
+     name : 'Tasmiya Tabassum',
+     roll : 22,
+     age : 21,
+     class : 'Five',
+     gender : 'Female',
+     Location : 'Jamil Nagor',
+     Admission_fee : 1500
+  },
+  {
+     name : 'Tania Tasnim',
+     roll : 23,
+     age : 29,
+     class : 'Ten',
+     gender : 'Female',
+     Location : 'Jamil Nagor',
+     Admission_fee : 2000
+  },
+  {
+     name : 'Nibor Roy',
+     roll : 24,
+     age : 11,
+     class : 'Five',
+     gender : 'Male',
+     Location : 'Johurul Nagor',
+     Admission_fee : 1500
+  },
+  {
+     name : 'Akash Islam',
+     roll : 25,
+     age : 12,
+     class : 'Seven',
+     gender : 'Male',
+     Location : 'Sobujbag',
+     Admission_fee : 1700
+  },
+  {
+     name : 'Ripa Paul',
+     roll : 26,
+     age : 12,
+     class : 'Five',
+     gender : 'Female',
+     Location : 'Jamil Nagor',
+     Admission_fee : 1500
+  },
+  {
+     name : 'Fatema Akter',
+     roll : 27,
+     age : 21,
+     class : 'Nine',
+     gender : 'Female',
+     Location : 'Jamil Nagor',
+     Admission_fee : 1900
+  },
+  {
+     name : 'Limon chawdhory',
+     roll : 28,
+     age : 17,
+     class : 'Eight',
+     gender : 'Male',
+     Location : 'Sobujbag',
+     Admission_fee : 1800
+  },
+  {
+     name : 'Israt Jahan',
+     roll : 29,
+     age : 27,
+     class : 'Six',
+     gender : 'Female',
+     Location : 'Jamil Nagor',
+     Admission_fee : 1600
+  },
+  {
+     name : 'Priya Islam',
+     roll : 30,
+     age : 26,
+     class : 'Ten',
+     gender : 'Female',
+     Location : 'JJohurul Nagor',
+     Admission_fee : 2000
+  },
+
+]
